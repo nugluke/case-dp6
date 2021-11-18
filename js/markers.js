fa820fa47afe645c038b68f205b5421cbe4040a0
nugluke <lucavemerick@gmail.com>
2021-11-18 20:58:50 -0300

    initial commit

---
+++ js/markers.js
@@ -0,0 +1,4 @@
+const MONITORING_MARKER = 'isTagged'
+const CATEGORY_MARKER = 'tagCategory'
+const ACTION_MARKER = 'tagAction'
+const LABEL_MARKER = 'tagLabel'
---
+++ js/tagueamento.js
@@ -0,0 +1,24 @@
+
+const sendTagEvent = (event) => {
+    const target = event.currentTarget
+    const category = target.getAttribute(CATEGORY_MARKER);
+    const action = target.getAttribute(ACTION_MARKER);
+    const label = target.getAttribute(LABEL_MARKER);
+    ga('send', 'event', category, action, label);
+}
+
+const addSendTagEvent = (el) => {
+    const eventType = (el.getAttribute('eventType'));
+    el.addEventListener(eventType , sendTagEvent, false);
+    
+}
+
+const appendListenerToElements = () => {
+    const elementsToTag = document.querySelectorAll(`[${MONITORING_MARKER}]`)
+    elementsToTag.forEach((el) => addSendTagEvent(el))
+    
+}
+
+window.onload = function() {
+    appendListenerToElements()
+  };
